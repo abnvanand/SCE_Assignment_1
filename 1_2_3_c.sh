8b250c3591c4b62adc7d61c8a9acb170909b0f18
Abhinav Anand <abhinav_anand@live.com>
2018-09-12 10:04:34 +0530

    Make mac addr regex shorter

--- 1_2_3_c.sh
+++ 1_2_3_c.sh
@@ -1,1 +1,3 @@
-ifconfig | grep -o -w -i -E "[0-9a-f]{2}(-|:)[0-9a-f]{2}(-|:)[0-9a-f]{2}(-|:)[0-9a-f]{2}(-|:)[0-9a-f]{2}(-|:)[0-9a-f]{2}"
+#ifconfig | grep -o -w -i -E "[0-9a-f]{2}(-|:)[0-9a-f]{2}(-|:)[0-9a-f]{2}(-|:)[0-9a-f]{2}(-|:)[0-9a-f]{2}(-|:)[0-9a-f]{2}"
+# we can have grouping inside grouping
+ifconfig |grep -o -w -i -E "([0-9a-f]{2}(-|:)){5}[0-9a-f]{2}"
