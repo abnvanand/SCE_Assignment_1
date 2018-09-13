2ace96c0a116e397ad686ca3cd25dad132df5bd3
Abhinav Anand <abhinav_anand@live.com>
2018-09-13 09:39:57 +0530

    Wrap up 1_2_2

---
+++ 1_2_2_a.sh
@@ -0,0 +1,2 @@
+# just the grep command without any inputs
+grep  -Ewo "^([[:digit:]]{3}\.){3}[[:digit:]]{3}$"
--- TODO_1_2_2_a.sh
+++
@@ -1,5 +0,0 @@
-# TODO: Ask should i write only the regex in this file
-# or the entire grep command
-# what's the input if I write the grep command?
-# FIXME: currently uses a custom file for input
-cat 1_2_2.txt | grep  -Ewo "^([[:digit:]]{3}\.){3}[[:digit:]]{3}$"
